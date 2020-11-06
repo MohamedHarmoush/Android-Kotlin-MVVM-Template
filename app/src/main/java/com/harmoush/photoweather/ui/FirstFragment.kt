2d0c60e76ad3d76818b0693681e62599e6f90506
mohamadKotb <mkotb@identity-solutions.org>
2020-11-06 14:52:04 +0200

    Initial commit

---
+++ app/src/main/java/com/harmoush/photoweather/ui/FirstFragment.kt
@@ -0,0 +1,33 @@
+package com.harmoush.photoweather.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.harmoush.photoweather.R
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class FirstFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.button_first).setOnClickListener {
+            Toast.makeText(requireContext(), "Go Next", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
